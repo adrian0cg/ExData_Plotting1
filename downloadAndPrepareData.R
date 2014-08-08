2a9d68d9c99fe1471fb224412feea498c6250aa1
Adrian Hass <adrian.hass@gmail.com>
2014-08-08 05:12:38 +0200

    download and prepare data

---
+++ downloadAndPrepareData.R
@@ -0,0 +1,48 @@
+downloadAndPrepareData <- function() {
+    # download
+    remoteFileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    localZipFileName <- "household_power_consumption.zip"
+    if (!file.exists(localZipFileName)) {
+        download.file(remoteFileUrl, localZipFileName, method="curl")
+    }
+    # unzip
+    localFileName <- "household_power_consumption.txt"
+    if (!file.exists(localFileName)){
+        unzip(localZipFileName, overwrite=FALSE)
+    }
+    # load
+    if (
+        !exists("houseHoldPowerConsumption")
+        || is.null(householdPowerConsumption)
+        || dim(householdPowerConsumption)[1]==2075259
+        ) {
+        householdPowerConsumption <<- read.table(
+            localFileName,
+            col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+            skip = 66637,
+            nrows = 2880,
+            sep = ";",
+            na.strings = "?",
+            colClasses = c(rep("character",2),rep("numeric",7))
+        )
+    }
+    # convert to timstamp
+    if (dim(householdPowerConsumption)[2] < 10) {
+        householdPowerConsumption <<- cbind(
+            householdPowerConsumption, DateTimeString = paste(
+                householdPowerConsumption$Date,
+                householdPowerConsumption$Time,
+                sep=" "
+            )
+        )
+    }
+    if (dim(householdPowerConsumption)[2] < 11) {
+        householdPowerConsumption <<- cbind(
+            householdPowerConsumption, Timestamp = strptime(
+                householdPowerConsumption$DateTimeString,
+                format="%d/%m/%Y %H:%M:%S"
+            )
+        )
+    }
+}
+downloadAndPrepareData()
